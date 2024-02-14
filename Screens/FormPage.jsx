745d3474935f9709e08cfa5795e5d077e40ca1c9
shawnmarkus <shawnmarkus4200@gmail.com>
2024-02-15 01:10:24 +0530

    dummy form page

---
+++ Screens/FormPage.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import ContainerWrapper from '../components/ContainerWrapper';
+
+function FormPage() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <ContainerWrapper>
+      <Text>Form data </Text>
+    </ContainerWrapper>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default FormPage;
