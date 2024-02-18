92e2171594372493d64aaae7da7cb8d1c2116b55
shawnmarkus <shawnmarkus4200@gmail.com>
2024-02-19 01:53:44 +0530

    home page card has been made

--- Screens/HomeScreen.jsx
+++ Screens/HomeScreen.jsx
@@ -1,5 +1,13 @@
-import React from 'react';
-import {Pressable, StyleSheet, Text, useColorScheme, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  Animated,
+  Pressable,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  useColorScheme,
+  View,
+} from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import ContainerWrapper from '../components/ContainerWrapper';
@@ -15,24 +23,50 @@ function HomeScreen({navigation}) {
 
   const data = [
     {
-      name: 'ss',
-      age: 22,
+      name: 'ABC', //social.name
+      fatherName: 'DEF', //social.familyDetails.fatherName
+      motherName: 'GHI', //social.familyDetails.motherName
+      DOB: 'J/K/L', //social.DOB
+      address: 'M/N/O', //social.address.homeAddress
+      contactNo: '1234567890', //social.phoneNo
+      gender: 'M/A', //social.sec
+    },
+
+    {
+      name: 'ABC', //social.name
+      fatherName: 'DEF', //social.familyDetails.fatherName
+      motherName: 'GHI', //social.familyDetails.motherName
+      DOB: 'J/K/L', //social.DOB
+      address: 'M/N/O', //social.address.homeAddress
+      contactNo: '1234567890', //social.phoneNo
+      gender: 'M/A', //social.sec
     },
     {
-      name: 'ps',
-      age: 23,
+      name: 'ABC', //social.name
+      fatherName: 'DEF', //social.familyDetails.fatherName
+      motherName: 'GHI', //social.familyDetails.motherName
+      DOB: 'J/K/L', //social.DOB
+      address: 'M/N/O', //social.address.homeAddress
+      contactNo: '1234567890', //social.phoneNo
+      gender: 'M/A', //social.sec
     },
     {
-      name: 'gy',
-      age: 22,
+      name: 'ABC', //social.name
+      fatherName: 'DEF', //social.familyDetails.fatherName
+      motherName: 'GHI', //social.familyDetails.motherName
+      DOB: 'J/K/L', //social.DOB
+      address: 'M/N/O', //social.address.homeAddress
+      contactNo: '1234567890', //social.phoneNo
+      gender: 'M/A', //social.sec
     },
     {
-      name: 'pa',
-      age: 25,
-    },
-    {
-      name: 'av',
-      age: 27,
+      name: 'ABC', //social.name
+      fatherName: 'DEF', //social.familyDetails.fatherName
+      motherName: 'GHI', //social.familyDetails.motherName
+      DOB: 'J/K/L', //social.DOB
+      address: 'M/N/O', //social.address.homeAddress
+      contactNo: '1234567890', //social.phoneNo
+      gender: 'M/A', //social.sec
     },
   ];
 
@@ -42,8 +76,13 @@ function HomeScreen({navigation}) {
   return (
     <ContainerWrapper>
       <View style={styles.fullHeight}>
+        <Text style={styles.headingText}>List of Registered Patient</Text>
         <FlatList
           data={data}
+          contentContainerStyle={{
+            height: '100%',
+            padding: 20,
+          }}
           style={{height: '100%'}}
           renderItem={({item, idx}) => {
             return <RenderItem item={item} idx={idx} />;
@@ -59,15 +98,106 @@ function HomeScreen({navigation}) {
   );
 }
 
-const RenderItem = ({item, idx}) => (
-  <Pressable>
-    <Text style={{color: 'black'}}>
-      {item?.name} == {item?.age}
-    </Text>
-  </Pressable>
-);
+const RenderItem = ({item, idx}) => {
+  const [isDetailsVisible, setIsDetailsVisible] = useState(false);
+  return (
+    <Pressable style={styles.masterWrapperOfSubComp}>
+      <View style={styles.flexRowContainer}>
+        <View>
+          <Text style={styles.boldName}>{item?.name}</Text>
+
+          <Text style={{color: '#000', lineHeight: 25}}>S/o</Text>
+
+          <Text style={{color: '#000'}}>
+            dhshdadhkadahjk{' '}
+            <Text style={{fontStyle: 'italic', fontSize: 10}}>
+              (father name)
+            </Text>
+          </Text>
+        </View>
+
+        {!isDetailsVisible && (
+          <TouchableOpacity
+            onPress={() => {
+              setIsDetailsVisible(true);
+            }}
+            style={{alignSelf: 'flex-end'}}>
+            <Text style={{fontSize: 12, fontStyle: 'italic', color: 'grey'}}>
+              ...More
+            </Text>
+          </TouchableOpacity>
+        )}
+      </View>
+
+      {isDetailsVisible && (
+        <Animated.View>
+          <View>
+            <Text style={{color: 'black', paddingVertical: 5}}>
+              Motther name{' '}
+              <Text style={{fontStyle: 'italic', fontSize: 10}}>
+                (Mother name)
+              </Text>{' '}
+            </Text>
+            <View style={styles.padJustifyFlexRow}>
+              <Text style={{color: 'black'}}>Mobile: 78887774367</Text>
+              <Text style={{color: 'black'}}>DOB: 12/12/2002</Text>
+              <Text style={{color: 'black'}}> MALE</Text>
+            </View>
+            <Text style={{color: 'black'}}>
+              Address :
+              {
+                'dhjksdhsdjkafjkfhd dsajdhjk dsadsahdjk dsahdjahdjkdj dhjksahdasdj d ajkdhadhk  hdkj h ksdjsdjkdjlad djsddjal fhdjkfjkdfj fjdsfjkhfs fkjdjsfjkhdfjh'
+              }
+            </Text>
+          </View>
+
+          <TouchableOpacity
+            style={{alignSelf: 'flex-end'}}
+            onPress={() => {
+              setIsDetailsVisible(false);
+            }}>
+            <Text style={styles.showLess}>Show Less</Text>
+          </TouchableOpacity>
+        </Animated.View>
+      )}
+    </Pressable>
+  );
+};
 
 const styles = StyleSheet.create({
+  headingText: {
+    color: 'black',
+    paddingHorizontal: 30,
+    paddingTop: 20,
+    fontSize: 24,
+    fontWeight: '800',
+    textAlign: 'center',
+  },
+  showLess: {
+    fontSize: 12,
+    fontStyle: 'italic',
+    color: 'grey',
+    paddingTop: 10,
+  },
+  padJustifyFlexRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 5,
+  },
+  boldName: {
+    color: 'black',
+    fontSize: 20,
+    fontWeight: '600',
+  },
+  flexRowContainer: {flexDirection: 'row', justifyContent: 'space-between'},
+  masterWrapperOfSubComp: {
+    marginVertical: 10,
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    borderRadius: 5,
+    backgroundColor: '#fff',
+    elevation: 5,
+  },
   floatingBtn: {
     backgroundColor: 'black',
     width: 60,
@@ -76,7 +206,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderRadius: 100,
     position: 'absolute',
-    bottom: 50,
+    bottom: 60,
     right: 40,
   },
   fullHeight: {
